4f422b2d6cba3eaaed50ea59ea23b44d0e7bf762
Len Creemers <len.creemers@gmail.com>
2024-12-12 16:07:42 +0100

    day12 part two

--- day_12/part_two.py
+++ day_12/part_two.py
@@ -9,30 +9,50 @@ def add_padding(matrix):
     return padding_row + padded_matrix + padding_row
 
 def explore(char, i, j):
-    global area, perimeter
-    if not input[i][j] == char:
-        perimeter += 1
-        return
-    
+    global area, sides   
     visited.add((i,j))
-    global_visited.add((i, j))
+    global_visited.add((i,j))
     area += 1
-    for (dy, dx) in directions:
-        if not (i + dy, j + dx) in visited:
+    for (dy,dx) in directions:
+        if input[i + dy][j + dx] != char:
+            sides += [((dy,dx), (i,j))]
+        elif not (i + dy, j + dx) in visited:
             explore(char, i + dy, j + dx)
-    
 
+def calculate_sides(sides):
+    unique_sides = 0
+    while len(sides) > 0:
+        current_side = sides[0]
+        sides = explore_side(current_side, sides[0:])
+        unique_sides += 1
+        if current_side in sides: sides.remove(current_side)
+    return unique_sides
+        
+def explore_side(side, sides):
+    if side[0] == (1, 0) or side[0] == (-1, 0):
+        targets = [(side[0], (side[1][0], side[1][1] - 1)), (side[0], (side[1][0], side[1][1] + 1))]
+        for target in targets:
+            if target in sides:
+                sides.remove(target)
+                sides = explore_side(target, sides)
+    elif side[0] == (0, 1) or side[0] == (0, -1):
+        targets = [(side[0], (side[1][0] + 1, side[1][1])), (side[0], (side[1][0] - 1, side[1][1]))]
+        for target in targets:
+            if target in sides:
+                sides.remove(target)
+                sides = explore_side(target, sides)
+    return sides
+            
 input = [list(line) for line in add_padding(input)]
 global_visited = set()
 
 for i in range(len(input)):
     for j in range(len(input[i])):
-        if not input[i][j] == "!" and not (i, j) in global_visited:
+        if input[i][j] != "!" and not (i, j) in global_visited:
             visited = set()
             area = 0
-            perimeter = 0
+            sides = []
             explore(input[i][j], i, j)
-            total += area * perimeter
-
+            total += area * calculate_sides(sides)
 print(f"The total price of fencing all the regions is {total}")
 
